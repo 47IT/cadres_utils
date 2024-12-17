52ec34ed60d6445ed273a261f8870b2468f50559
kashim <dmetlushko@gmail.com>
2024-12-17 16:06:05 +0200

    initial

--- cadres_utils/field_utils.py
+++ cadres_utils/field_utils.py
@@ -4,10 +4,13 @@ import pandas as pd
 from numpy import int64
 
 
-def get_field_value(curr_row, field_name, is_float_to_int: bool = False):
+def get_field_value(curr_row, field_name, is_float_to_int: bool = False, custom_date_format: str = None):
     if pd.notnull(curr_row[field_name]):
         if isinstance(curr_row[field_name], (datetime, pd.Timestamp)):
-            return date_to_str(curr_row[field_name])
+            date_format = '%d.%m.%Y'
+            if custom_date_format:
+                date_format = custom_date_format
+            return date_to_str(curr_row[field_name], date_format)
         elif isinstance(curr_row[field_name], int64):
             return str(curr_row[field_name])
         elif is_float_to_int and isinstance(curr_row[field_name], float):
@@ -17,9 +20,9 @@ def get_field_value(curr_row, field_name, is_float_to_int: bool = False):
         return None
 
 
-def date_to_str(date: pd.Timestamp) -> str | None:
+def date_to_str(date: pd.Timestamp, date_format: str = '%d.%m.%Y') -> str | None:
     if date and pd.notnull(date) and (isinstance(date, pd.Timestamp) or isinstance(date, datetime)):
-        return date.strftime('%d.%m.%Y')
+        return date.strftime(date_format)
     elif isinstance(date, str):
         return date
     return None
