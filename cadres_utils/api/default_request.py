2fb9d7f2729e6d464d689da5612dcdacbdd9dcca
kashim <dmetlushko@gmail.com>
2025-03-14 12:24:02 +0200

    initial

--- cadres_utils/api/default_request.py
+++ cadres_utils/api/default_request.py
@@ -44,7 +44,8 @@ async def process_default_list(
     if not df.empty and date_fields:
         for field in date_fields:
             try:
-                df[field] = df[field].apply(lambda x: pd.to_datetime(x, format='ISO8601') if pd.notnull(x) else None)
+                # df[field] = df[field].apply(lambda x: pd.to_datetime(x, format='ISO8601') if pd.notnull(x) else None)
+                df[field] = pd.to_datetime(df[field], format='ISO8601', errors='coerce')
             except Exception as e:
                 logging.error(f'Error in field {field} of {object_name}')
                 raise e
