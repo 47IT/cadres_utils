bc0da4d5c369bdec60aa7a1d8a92cdb7275d8d04
kashim <dmetlushko@gmail.com>
2025-09-04 18:12:37 +0300

    add Ukrainian number-to-text conversion function with gender support

---
+++ cadres_utils/language_ua_utils.py
@@ -0,0 +1,44 @@
+from enum import Enum
+
+HUNDRED = ['сто', 'двісти', 'триста', 'чотириста', 'п\'ятсот', 'шістсот', 'сімсот', 'вісімсот', 'дев\'ятсот']
+TEN = ['двадцять', 'тридцять', 'сорок', 'п\'ятьдесят', 'шістьдесят', 'сімдесят', 'вісімдесят', 'дев\'яносто']
+SINGLE_MALE = ['нуль', 'один', 'два', 'три', 'чотири', 'п\'ять', 'шість', 'сім', 'вісім', 'дев\'ять']
+SINGLE_FEMALE = ['нуль', 'одна', 'дві', 'три', 'чотири', 'п\'ять', 'шість', 'сім', 'вісім', 'дев\'ять']
+TEEN = ['десять', 'одинадцять', 'дванадцять', 'тринадцять', 'чотирнадцять', 'п\'ятнадцять', 'шістнадцять', 'сімнадцять', 'вісімнадцять', 'дев\'ятнадцять']
+
+
+class Gender(Enum):
+    MALE = 'male'
+    FEMALE = 'female'
+
+
+def int_2_text_ukraine(int_val: int, gender: Gender) -> str:
+    def __calc_less_100(val_2_proc: int) -> str:
+        ten_val = val_2_proc // 10
+        unit_val = val_2_proc % 10
+
+        return f'{TEN[ten_val - 2]} {single_list[unit_val]}'
+
+    if gender == Gender.MALE:
+        single_list = SINGLE_MALE
+    else:
+        single_list = SINGLE_FEMALE
+
+    if int_val < 10:
+        res = single_list[int_val]
+    elif int_val < 20:
+        res = TEEN[int_val - 10]
+    elif int_val < 100:
+        res = __calc_less_100(int_val)
+    elif int_val < 1000:
+        hundred_val = int_val // 100
+        rest_val = int_val % 100
+        if rest_val == 0:
+            res = HUNDRED[hundred_val - 1]
+        else:
+            res = f'{HUNDRED[hundred_val - 1]} {__calc_less_100(rest_val)}'
+    else:
+        raise ValueError('Підтримуються лише числа до 1000')
+
+    return res
+
