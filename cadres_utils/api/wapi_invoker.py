9a9ba052d6fb35264b023f2ba73c15fd651d4a10
kashim <dmetlushko@gmail.com>
2025-09-18 15:48:06 +0300

    Add JWT support to WapiInvoker and refactor for improved header management

--- cadres_utils/api/wapi_invoker.py
+++ cadres_utils/api/wapi_invoker.py
@@ -1,24 +1,22 @@
+import json
+import time
+
 import aiohttp
+import jwt
+from jwt import PyJWK
 
 from cadres_utils.api.exception import ApiException, ApiUnauthorizedException
 
 
 class WapiInvoker:
-    def __init__(self, host: str, auth_token: str | None, jwt_token: str | None = None):
+    def __init__(self, host: str, auth_token: str):
         self.__auth_token = auth_token
-        self.__jwt_token = jwt_token
         self.__host = host
 
     async def post_request(self, object_operation, request_body) -> dict:
         url = self.get_wapi_base_url() + object_operation
 
-        headers = {
-            "Content-Type": "application/json"
-        }
-        if self.__jwt_token:
-            headers["Authorization"] = f"Bearer {self.__jwt_token}"
-        if self.__auth_token:
-            headers["Cookie"] = f"sid={self.__auth_token}"
+        headers = self._get_headers()
 
         async with aiohttp.ClientSession(headers=headers) as session:
             async with session.post(url, json=request_body, ssl=False) as response:
@@ -35,3 +33,42 @@ class WapiInvoker:
 
     def get_cookies(self):
         return {'sid': self.__auth_token}
+
+    def _get_headers(self):
+        headers = {
+            "Content-Type": "application/json"
+        }
+        if self.__auth_token:
+            headers["Cookie"] = f"sid={self.__auth_token}"
+
+        return headers
+
+
+class JWTWapiInvoker(WapiInvoker):
+    def __init__(self, host: str, jwk_private_key_path: str):
+        super().__init__(host=host, auth_token='')
+        self.__jwk_private_key_path = jwk_private_key_path
+        self.__jwk_data = self.__load_jwk()
+        self.__private_key = PyJWK(jwk_data=self.__jwk_data, algorithm='RS256')
+
+
+    def _get_headers(self):
+        headers = super()._get_headers()
+        headers['Authorization'] = f'Bearer {self.__get_token()}'
+
+    def __load_jwk(self):
+        with open(self.__jwk_private_key_path, 'r') as f:
+            jwk_data = json.load(f)
+        return jwk_data
+
+    def __get_token(self) -> str:
+        payload = {
+            'sub': self.__jwk_data['sub'],
+            'aud': self.__jwk_data['aud'],
+            "iat": int(time.time()),
+            "iss": self.__jwk_data['iss'],
+        }
+
+        token = jwt.encode(payload, self.__private_key, algorithm="RS256")
+
+        return token
