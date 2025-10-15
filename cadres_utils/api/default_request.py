a103f0e438b185cf8e36408a73a1382f39b8fec7
hnatt <hnatt88@gmail.com>
2025-10-15 16:00:35 +0300

    Round seconds for logging

--- cadres_utils/api/default_request.py
+++ cadres_utils/api/default_request.py
@@ -74,7 +74,7 @@ async def process_default_list(
                 raise e
 
     end_time = time.perf_counter()
-    logging.debug(f'Finish fetching data {object_name} - work time: {end_time - start_time} sec')
+    logging.debug(f'{object_name} DONE: {(end_time - start_time):.1f}s')
     return df
 
 
