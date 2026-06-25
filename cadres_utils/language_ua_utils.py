5c1251956ac0c2213f0c9455753f015ef9278b5a
hnatt <hnatt88@gmail.com>
2026-06-25 12:45:44 +0300

    fix int_2_text_ukraine

--- cadres_utils/language_ua_utils.py
+++ cadres_utils/language_ua_utils.py
@@ -1,47 +1,98 @@
 from enum import Enum
 
-HUNDRED = ['сто', 'двісті', 'триста', 'чотириста', 'п\'ятсот', 'шістсот', 'сімсот', 'вісімсот', 'дев\'ятсот']
-TEN = ['двадцять', 'тридцять', 'сорок', 'п\'ятьдесят', 'шістьдесят', 'сімдесят', 'вісімдесят', 'дев\'яносто']
-SINGLE_MALE = ['нуль', 'один', 'два', 'три', 'чотири', 'п\'ять', 'шість', 'сім', 'вісім', 'дев\'ять']
-SINGLE_FEMALE = ['нуль', 'одна', 'дві', 'три', 'чотири', 'п\'ять', 'шість', 'сім', 'вісім', 'дев\'ять']
-TEEN = ['десять', 'одинадцять', 'дванадцять', 'тринадцять', 'чотирнадцять', 'п\'ятнадцять', 'шістнадцять', 'сімнадцять', 'вісімнадцять', 'дев\'ятнадцять']
-
-
 class Gender(Enum):
     MALE = 'male'
     FEMALE = 'female'
 
 
-def int_2_text_ukraine(int_val: int, gender: Gender) -> str:
-    def __calc_less_100(val_2_proc: int) -> str:
-        ten_val = val_2_proc // 10
-        unit_val = val_2_proc % 10
+_ONES = {
+    1: {Gender.MALE: "один", Gender.FEMALE: "одна"},
+    2: {Gender.MALE: "два", Gender.FEMALE: "дві"},
+    3: "три",
+    4: "чотири",
+    5: "п’ять",
+    6: "шість",
+    7: "сім",
+    8: "вісім",
+    9: "дев’ять",
+}
 
-        num_text = TEN[ten_val - 2]
-        if unit_val > 0:
-            num_text += f' {single_list[unit_val]}'
-        return num_text
+_TEENS = {
+    10: "десять",
+    11: "одинадцять",
+    12: "дванадцять",
+    13: "тринадцять",
+    14: "чотирнадцять",
+    15: "п’ятнадцять",
+    16: "шістнадцять",
+    17: "сімнадцять",
+    18: "вісімнадцять",
+    19: "дев’ятнадцять",
+}
 
-    if gender == Gender.MALE:
-        single_list = SINGLE_MALE
+_TENS = {
+    2: "двадцять",
+    3: "тридцять",
+    4: "сорок",
+    5: "п’ятдесят",
+    6: "шістдесят",
+    7: "сімдесят",
+    8: "вісімдесят",
+    9: "дев’яносто",
+}
+
+_HUNDREDS = {
+    1: "сто",
+    2: "двісті",
+    3: "триста",
+    4: "чотириста",
+    5: "п’ятсот",
+    6: "шістсот",
+    7: "сімсот",
+    8: "вісімсот",
+    9: "дев’ятсот",
+}
+
+
+def _under_1000(n: int, gender: Gender) -> list[str]:
+    words = []
+    h, rem = divmod(n, 100)
+    if h:
+        words.append(_HUNDREDS[h])
+    if 10 <= rem <= 19:
+        words.append(_TEENS[rem])
     else:
-        single_list = SINGLE_FEMALE
+        t, o = divmod(rem, 10)
+        if t:
+            words.append(_TENS[t])
+        if o:
+            val = _ONES[o]
+            words.append(val[gender] if isinstance(val, dict) else val)
+    return words
 
-    if int_val < 10:
-        res = single_list[int_val]
-    elif int_val < 20:
-        res = TEEN[int_val - 10]
-    elif int_val < 100:
-        res = __calc_less_100(int_val)
-    elif int_val < 1000:
-        hundred_val = int_val // 100
-        rest_val = int_val % 100
-        if rest_val == 0:
-            res = HUNDRED[hundred_val - 1]
-        else:
-            res = f'{HUNDRED[hundred_val - 1]} {__calc_less_100(rest_val)}'
-    else:
-        raise ValueError('Підтримуються лише числа до 1000')
 
-    return res
+def _thousand_word(count: int) -> str:
+    if count == 1:
+        return "тисяча"
+    if count in (2, 3, 4):
+        return "тисячі"
+    return "тисяч"
 
+
+def number_to_words(value: int, gender: Gender) -> str:
+    if not isinstance(value, int) or isinstance(value, bool) or not 0 <= value <= 9999:
+        return str(value)
+    if value == 0:
+        return "нуль"
+
+    words = []
+    thousands, rest = divmod(value, 1000)
+    if thousands:
+        words += _under_1000(thousands, Gender.FEMALE)  # тисяча is feminine
+        words.append(_thousand_word(thousands))
+    if rest:
+        words += _under_1000(rest, gender)
+    return " ".join(words)
+
+# DEPRECATED: use new name unless you're 13 years old who's 2cool4school
+int_2_text_ukraine=number_to_words
